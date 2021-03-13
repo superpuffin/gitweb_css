33b9db071f8293fb063cb6ecf641a917ca2f821a
Yorgos Bos <yorgos.bos@outlook.com>
2021-03-13 11:53:57 +0100

    upgrade gulp

---
+++ gulpfile.esm.js
@@ -0,0 +1,43 @@
+/*eslint-env node*/
+
+import { src, dest, series } from "gulp";
+
+const sass = require("gulp-dart-sass");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const del = require("del");
+const rename = require("gulp-rename");
+const postcssNormalize = require("postcss-normalize");
+
+export function clean(done) {
+  del.sync("dist");
+  done();
+}
+
+export function build_cssmin() {
+  var plugins = [postcssNormalize(), autoprefixer({}), cssnano()];
+  return (
+    src("src/**/*.scss")
+      .pipe(sourcemaps.init())
+      .pipe(sass().on("error", sass.logError)) // Using gulp-sass
+      // Minify and prefix css
+      .pipe(postcss(plugins))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(sourcemaps.write("."))
+      .pipe(dest("dist/"))
+  );
+}
+
+export function build_css() {
+  return src("src/**/*.scss")
+    .pipe(sass().on("error", sass.logError)) // Using gulp-sass
+    .pipe(postcss([postcssNormalize()]))
+    .pipe(dest("dist/"));
+}
+
+export default function defaultFunction(cb) {
+  series([clean, build_css, build_cssmin]);
+  cb()
+}
